52dae71c3d331b90dffcc5b14ed6f9dcc8cd8702
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-07-25 08:29:13 +0200

    Add missing packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,74 +1,74 @@
 module.exports = {
-	darkMode: 'class',
-	content: ['./resources/js/**/*.{ts,tsx}'],
+	darkMode: "class",
+	content: ["./resources/js/**/*.{ts,tsx}"],
 	theme: {
 		container: {
 			center: true,
-			padding: '2rem',
+			padding: "2rem",
 			screens: {
-				'2xl': '1400px',
+				"2xl": "1400px",
 			},
 		},
 		extend: {
 			animation: {
-				'accordion-down': 'accordion-down 0.2s ease-out',
-				'accordion-up': 'accordion-up 0.2s ease-out',
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
 			},
 			borderRadius: {
-				sm: 'calc(var(--radius) - 4px)',
-				md: 'calc(var(--radius) - 2px)',
-				lg: 'var(--radius)',
+				sm: "calc(var(--radius) - 4px)",
+				md: "calc(var(--radius) - 2px)",
+				lg: "var(--radius)",
 			},
 			colors: {
-				background: 'hsl(var(--background))',
-				border: 'hsl(var(--border))',
-				foreground: 'hsl(var(--foreground))',
-				input: 'hsl(var(--input))',
-				ring: 'hsl(var(--ring))',
+				background: "hsl(var(--background))",
+				border: "hsl(var(--border))",
+				foreground: "hsl(var(--foreground))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
 				accent: {
-					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))',
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
 				},
 				card: {
-					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))',
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
 				},
 				constructive: {
-					DEFAULT: 'hsl(var(--constructive))',
-					foreground: 'hsl(var(--constructive-foreground))',
+					DEFAULT: "hsl(var(--constructive))",
+					foreground: "hsl(var(--constructive-foreground))",
 				},
 				destructive: {
-					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))',
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
 				},
 				muted: {
-					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))',
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
 				},
 				popover: {
-					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))',
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
 				},
 				primary: {
-					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))',
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
 				},
 				secondary: {
-					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))',
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
 				},
 			},
 			keyframes: {
-				'accordion-down': {
-					from: { height: '0' },
-					to: { height: 'var(--radix-accordion-content-height)' },
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
 				},
-				'accordion-up': {
-					from: { height: 'var(--radix-accordion-content-height)' },
-					to: { height: '0' },
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
 				},
 			},
 		},
 	},
-	plugins: [require('@tailwindcss/typography')],
+	plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
