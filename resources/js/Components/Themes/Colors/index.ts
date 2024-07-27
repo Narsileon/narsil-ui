2071ae5dba305f232a3ec892b584a9e00edbb953
Narsilien <rigaux.jonathan@gmail.com>
2024-07-27 15:23:55 +0200

    Format colors

--- resources/js/Components/Themes/Colors/index.ts
+++ resources/js/Components/Themes/Colors/index.ts
@@ -1,11 +1,11 @@
-import { blue } from './blue';
-import { gray } from './gray';
-import { green } from './green';
-import { neutral } from './neutral';
-import { orange } from './orange';
-import { pink } from './pink';
-import { red } from './red';
-import { violet } from './violet';
-import { yellow } from './yellow';
+import blue from "./blue";
+import gray from "./gray";
+import green from "./green";
+import neutral from "./neutral";
+import orange from "./orange";
+import pink from "./pink";
+import red from "./red";
+import violet from "./violet";
+import yellow from "./yellow";
 
 export { blue, gray, green, neutral, orange, pink, red, violet, yellow };
