d531b30540cf609c7d76266dbf37efa821682002
Narsilien <rigaux.jonathan@gmail.com>
2024-07-27 15:58:00 +0200

    Fix heading classnames

--- resources/js/Components/Heading/Heading.tsx
+++ resources/js/Components/Heading/Heading.tsx
@@ -22,12 +22,13 @@ export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement>, 
 	level: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }
 
-const Heading = React.forwardRef<HTMLHeadingElement, HeadingProps>(({ level, ...props }, ref) => {
+const Heading = React.forwardRef<HTMLHeadingElement, HeadingProps>(({ className, level, variant, ...props }, ref) => {
 	const Comp = level;
 
 	return (
 		<Comp
 			ref={ref}
+			className={cn(headingVariants({ variant, className }))}
 			{...props}
 		/>
 	);
