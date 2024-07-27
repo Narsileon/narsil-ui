ff0d1e1243fbcfe9881cc452ecb67392df39029c
Narsilien <rigaux.jonathan@gmail.com>
2024-07-27 21:03:37 +0200

    Add charts

---
+++ resources/js/Components/Chart/index.ts
@@ -0,0 +1,8 @@
+import { useChart } from "./ChartContainer";
+import ChartContainer from "./ChartContainer";
+import ChartLegend from "./ChartLegend";
+import ChartLegendContent from "./ChartLegendContent";
+import ChartTooltip from "./ChartTooltip";
+import ChartTooltipContent from "./ChartTooltipContent";
+
+export { ChartContainer, ChartLegend, ChartLegendContent, ChartTooltip, ChartTooltipContent, useChart };
