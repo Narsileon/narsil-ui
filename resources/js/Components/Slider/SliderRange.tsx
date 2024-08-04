eb5573837dca6865eababd7f2eb98ce7b6c6583b
Narsilien <rigaux.jonathan@gmail.com>
2024-08-04 16:05:02 +0200

    Add indicator to slider range

--- resources/js/Components/Slider/SliderRange.tsx
+++ resources/js/Components/Slider/SliderRange.tsx
@@ -7,25 +7,35 @@ const SliderRange = React.forwardRef<React.ElementRef<typeof SliderPrimitive.Roo
 		const values = props.value ?? [props.min, props.max];
 
 		return (
-			<SliderPrimitive.Root
-				ref={ref}
-				className={cn("relative flex h-10 w-full touch-none select-none items-center", className)}
-				{...props}
-			>
-				<SliderPrimitive.Track className='relative h-2 w-full grow overflow-hidden rounded-full bg-secondary'>
-					<SliderPrimitive.Range className='absolute h-full bg-primary' />
-				</SliderPrimitive.Track>
-				{values.map((value, index) => (
-					<SliderPrimitive.Thumb
-						className={cn(
-							"block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors",
-							"focus-visible:border-primary focus-visible:outline-none",
-							"disabled:pointer-events-none disabled:opacity-50"
-						)}
-						key={index}
-					/>
-				))}
-			</SliderPrimitive.Root>
+			<div>
+				<SliderPrimitive.Root
+					ref={ref}
+					className={cn("relative flex h-10 w-full touch-none select-none items-center", className)}
+					{...props}
+				>
+					<SliderPrimitive.Track className='relative h-2 w-full grow overflow-hidden rounded-full bg-secondary'>
+						<SliderPrimitive.Range className='absolute h-full bg-primary' />
+					</SliderPrimitive.Track>
+					{values.map((value, index) => (
+						<SliderPrimitive.Thumb
+							className={cn(
+								"relative block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors",
+								"focus-visible:border-primary focus-visible:outline-none",
+								"disabled:pointer-events-none disabled:opacity-50"
+							)}
+							key={index}
+						>
+							{value != props.min || value != props.max ? (
+								<span className='absolute left-0 right-0 top-7 mx-auto text-center'>{value}</span>
+							) : null}
+						</SliderPrimitive.Thumb>
+					))}
+				</SliderPrimitive.Root>
+				<div className='flex items-center justify-between gap-x-4'>
+					<span>{props.min}</span>
+					<span>{props.max}</span>
+				</div>
+			</div>
 		);
 	}
 );
