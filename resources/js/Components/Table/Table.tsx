82bff93aa504bbc452fbc8efa8448baf7167a367
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-08-06 10:44:07 +0200

    Fix table resizing

--- resources/js/Components/Table/Table.tsx
+++ resources/js/Components/Table/Table.tsx
@@ -2,10 +2,10 @@ import { cn } from "@narsil-ui/Components";
 import * as React from "react";
 
 const Table = React.forwardRef<HTMLTableElement, TableProps>(({ className, ...props }, ref) => (
-	<div className='relative w-full overflow-auto'>
+	<div className='w-max rounded-md border'>
 		<table
 			ref={ref}
-			className={cn("w-full caption-bottom text-sm", className)}
+			className={cn("table-auto caption-bottom text-sm", className)}
 			{...props}
 		/>
 	</div>
