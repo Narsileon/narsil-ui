865d02ae54882565833941ed7234d69c9d1569f2
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-08-07 11:08:37 +0200

    Correct table width

--- resources/js/Components/Table/Table.tsx
+++ resources/js/Components/Table/Table.tsx
@@ -2,7 +2,7 @@ import { cn } from "@narsil-ui/Components";
 import * as React from "react";
 
 const Table = React.forwardRef<HTMLTableElement, TableProps>(({ className, ...props }, ref) => (
-	<div className='w-max rounded-md border'>
+	<div className='min-w-max rounded-md border'>
 		<table
 			ref={ref}
 			className={cn("table-auto caption-bottom text-sm", className)}
