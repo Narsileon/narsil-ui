c2549c35309b738c1c7c68fccfaa641fc7379167
Narsilien <rigaux.jonathan@gmail.com>
2024-08-13 01:58:17 +0200

    Revert "Fix navigation design"
    This reverts commit 3365d640f3761edf77e700749a5c9380acf1c1ee.

--- resources/js/Components/NavigationMenu/NavigationMenuTrigger.tsx
+++ resources/js/Components/NavigationMenu/NavigationMenuTrigger.tsx
@@ -6,7 +6,7 @@ import * as React from "react";
 
 export const navigationMenuTriggerStyle = cva(
 	cn(
-		"group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-3 py-2 text-sm font-medium transition-colors gap-x-4",
+		"group inline-flex h-9 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors",
 		"hover:bg-accent hover:text-accent-foreground",
 		"focus:bg-accent focus:text-accent-foreground focus:outline-none",
 		"disabled:pointer-events-none disabled:opacity-50",
