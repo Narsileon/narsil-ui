8b7bb3e934f63ce56f1c729a3b744015708b9ac6
Narsilien <rigaux.jonathan@gmail.com>
2024-08-12 19:35:50 +0200

    Fix popover width

--- resources/js/Components/Combobox/AsyncCombobox.tsx
+++ resources/js/Components/Combobox/AsyncCombobox.tsx
@@ -21,140 +21,139 @@ import {
 	Svg,
 } from "@narsil-ui/Components";
 
-const AsyncCombobox = React.forwardRef<
-	React.ElementRef<typeof PopoverPrimitive.Content>,
-	AsyncComboboxProps
->(({ fetch, labelKey = "label", preview, sort = true, ucFirst = true, value, valueKey = "value", onChange }, ref) => {
-	const { trans } = useTranslationsStore();
+const AsyncCombobox = React.forwardRef<React.ElementRef<typeof PopoverPrimitive.Content>, AsyncComboboxProps>(
+	({ fetch, labelKey = "label", preview, sort = true, ucFirst = true, value, valueKey = "value", onChange }, ref) => {
+		const { trans } = useTranslationsStore();
 
-	const [open, setOpen] = React.useState(false);
-	const [option, setOption] = React.useState<SelectOption | null>(null);
-	const [options, setOptions] = React.useState<SelectOption[]>([]);
+		const [open, setOpen] = React.useState(false);
+		const [option, setOption] = React.useState<SelectOption | null>(null);
+		const [options, setOptions] = React.useState<SelectOption[]>([]);
 
-	const [loading, setLoading] = React.useState(false);
-	const [search, setSearch] = React.useState<string>("");
+		const [loading, setLoading] = React.useState(false);
+		const [search, setSearch] = React.useState<string>("");
 
-	const getValueOption = (value: string | number) => {
-		return options?.find((option) => option[valueKey] === value)?.[labelKey] ?? value;
-	};
+		const getValueOption = (value: string | number) => {
+			return options?.find((option) => option[valueKey] === value)?.[labelKey] ?? value;
+		};
 
-	const fetchOptions = async (search: string) => {
-		try {
-			const response = await axios.get(fetch, {
-				params: {
-					search: search,
-				},
-			});
+		const fetchOptions = async (search: string) => {
+			try {
+				const response = await axios.get(fetch, {
+					params: {
+						search: search,
+					},
+				});
 
-			return response.data.data;
-		} catch (error) {
-			console.error("Error fetching options:", error);
+				return response.data.data;
+			} catch (error) {
+				console.error("Error fetching options:", error);
 
-			return [];
-		}
-	};
-
-	const debouncedFetchOptions = React.useCallback(
-		debounce(async (search) => {
-			if (search.length > 2) {
-				setLoading(true);
-
-				let options = await fetchOptions(search);
-
-				if (sort) {
-					options = sortBy(options, (option) => option[labelKey].toLowerCase());
-				}
-
-				setOptions(options);
-
-				setLoading(false);
-			} else {
-				setOptions([]);
+				return [];
 			}
-		}, 300),
-		[fetch, labelKey, sort]
-	);
+		};
 
-	React.useEffect(() => {
-		debouncedFetchOptions(search);
-	}, [search, debouncedFetchOptions]);
+		const debouncedFetchOptions = React.useCallback(
+			debounce(async (search) => {
+				if (search.length > 2) {
+					setLoading(true);
 
-	return (
-		<Popover
-			open={open}
-			onOpenChange={setOpen}
-		>
-			<PopoverTrigger asChild={true}>
-				<Button
-					aria-expanded={open}
-					className='w-full justify-start gap-x-2'
-					role='combobox'
-					variant='outline'
-				>
-					{option ? (
-						<>
-							{preview === "icon" ? <Svg src={`/storage/icons/${option?.[labelKey]}`} /> : null}
+					let options = await fetchOptions(search);
 
-							<span className='grow text-left'>
-								{ucFirst ? upperFirst(option?.[labelKey]) : getValueOption(option?.[labelKey])}
-							</span>
-						</>
-					) : (
-						trans("Select...")
-					)}
-					<ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
-				</Button>
-			</PopoverTrigger>
-			<PopoverContent
-				className='p-0'
-				align='start'
+					if (sort) {
+						options = sortBy(options, (option) => option[labelKey].toLowerCase());
+					}
+
+					setOptions(options);
+
+					setLoading(false);
+				} else {
+					setOptions([]);
+				}
+			}, 300),
+			[fetch, labelKey, sort]
+		);
+
+		React.useEffect(() => {
+			debouncedFetchOptions(search);
+		}, [search, debouncedFetchOptions]);
+
+		return (
+			<Popover
+				open={open}
+				onOpenChange={setOpen}
 			>
-				<Command shouldFilter={false}>
-					<CommandInput
-						placeholder={trans("Search...")}
-						className='h-9'
-						value={search}
-						onValueChange={setSearch}
-					/>
-					<CommandList
-						className='min-h-10'
-						key={options.length}
+				<PopoverTrigger asChild={true}>
+					<Button
+						aria-expanded={open}
+						className='w-full justify-start gap-x-2'
+						role='combobox'
+						variant='outline'
 					>
-						{loading ? <CommandLoading>{trans("Search...")}</CommandLoading> : null}
-						<CommandGroup>
-							{options.map((option, index) => {
-								const optionLabel = isString(option) ? option : option[labelKey];
-								const optionValue = isString(option) ? option : option[valueKey];
+						{option ? (
+							<>
+								{preview === "icon" ? <Svg src={`/storage/icons/${option?.[labelKey]}`} /> : null}
 
-								return (
-									<CommandItem
-										className='gap-x-2'
-										value={optionValue}
-										onSelect={() => {
-											onChange(optionValue);
-											setOption(option);
-											setOpen(false);
-										}}
-										key={optionValue}
-									>
-										{preview === "icon" ? <Svg src={`/storage/icons/${optionLabel}`} /> : null}
-										{ucFirst ? upperFirst(optionLabel) : optionLabel}
-										<Check
-											className={cn(
-												"ml-auto h-4 w-4",
-												value === optionValue ? "opacity-100" : "opacity-0"
-											)}
-										/>
-									</CommandItem>
-								);
-							})}
-						</CommandGroup>
-						<CommandEmpty>{!loading ? trans("No options.") : ""}</CommandEmpty>
-					</CommandList>
-				</Command>
-			</PopoverContent>
-		</Popover>
-	);
-});
+								<span className='grow text-left'>
+									{ucFirst ? upperFirst(option?.[labelKey]) : getValueOption(option?.[labelKey])}
+								</span>
+							</>
+						) : (
+							trans("Select...")
+						)}
+						<ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent
+					className='p-0'
+					align='start'
+				>
+					<Command shouldFilter={false}>
+						<CommandInput
+							placeholder={trans("Search...")}
+							className='h-9'
+							value={search}
+							onValueChange={setSearch}
+						/>
+						<CommandList
+							className='min-h-10'
+							key={options.length}
+						>
+							{loading ? <CommandLoading>{trans("Search...")}</CommandLoading> : null}
+							<CommandGroup>
+								{options.map((option, index) => {
+									const optionLabel = isString(option) ? option : option[labelKey];
+									const optionValue = isString(option) ? option : option[valueKey];
+
+									return (
+										<CommandItem
+											className='gap-x-2'
+											value={optionValue}
+											onSelect={() => {
+												onChange(optionValue);
+												setOption(option);
+												setOpen(false);
+											}}
+											key={optionValue}
+										>
+											{preview === "icon" ? <Svg src={`/storage/icons/${optionLabel}`} /> : null}
+											{ucFirst ? upperFirst(optionLabel) : optionLabel}
+											<Check
+												className={cn(
+													"ml-auto h-4 w-4",
+													value === optionValue ? "opacity-100" : "opacity-0"
+												)}
+											/>
+										</CommandItem>
+									);
+								})}
+							</CommandGroup>
+							<CommandEmpty>{!loading ? trans("No options.") : ""}</CommandEmpty>
+						</CommandList>
+					</Command>
+				</PopoverContent>
+			</Popover>
+		);
+	}
+);
 
 export default AsyncCombobox;
