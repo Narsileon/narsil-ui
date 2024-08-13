ac78fb876ddc7474bec06832627cf883b1d04950
Narsilien <rigaux.jonathan@gmail.com>
2024-08-13 02:02:48 +0200

    Fix input

--- resources/js/Components/Input/Input.tsx
+++ resources/js/Components/Input/Input.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 
 export const inputStyle = cva(
 	cn(
-		"border-input bg-background ring-offset-background flex h-9 w-full rounded-md border px-4 py-2 text-sm",
+		"border-input bg-background ring-offset-background flex h-10 w-full rounded-md border px-4 py-2 text-sm",
 		"placeholder:text-muted-foreground",
 		"file:border-0 file:bg-transparent file:text-sm file:font-medium",
 		"focus-visible:border-primary focus-visible:outline-none",
