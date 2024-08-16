6a3de7402e6ce8f7803e674bc1a856207d47b415
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-08-16 10:24:32 +0200

    Fix table width

--- resources/js/Components/Table/Table.tsx
+++ resources/js/Components/Table/Table.tsx
@@ -4,10 +4,10 @@ import * as React from "react";
 export interface TableProps extends React.HTMLAttributes<HTMLTableElement> {}
 
 const Table = React.forwardRef<HTMLTableElement, TableProps>(({ className, ...props }, ref) => (
-	<div className='min-w-max rounded-md border'>
+	<div className='rounded-md border'>
 		<table
 			ref={ref}
-			className={cn("table-auto caption-bottom text-sm", className)}
+			className={cn("min-w-max table-auto caption-bottom text-sm", className)}
 			{...props}
 		/>
 	</div>
