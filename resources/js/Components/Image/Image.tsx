5fe02c7979a57565a42b5a18ec41ebdb5b56dce1
Narsilien <rigaux.jonathan@gmail.com>
2024-08-24 20:39:17 +0200

    Remove preview from image

--- resources/js/Components/Image/Image.tsx
+++ resources/js/Components/Image/Image.tsx
@@ -1,42 +1,16 @@
 import { forwardRef } from "react";
 import * as React from "react";
-import Dialog from "@narsil-ui/Components/Dialog/Dialog";
-import DialogContent from "@narsil-ui/Components/Dialog/DialogContent";
-import DialogHeader from "@narsil-ui/Components/Dialog/DialogHeader";
-import DialogTitle from "@narsil-ui/Components/Dialog/DialogTitle";
-import DialogTrigger from "@narsil-ui/Components/Dialog/DialogTrigger";
 
 interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
 	caption?: string;
-	disabled?: boolean;
 }
 
-const Image = forwardRef<HTMLImageElement, ImageProps>(({ caption, disabled = false, src, ...props }, ref) => {
+const Image = forwardRef<HTMLElement, ImageProps>(({ caption, ...props }, ref) => {
 	return (
-		<Dialog>
-			<DialogTrigger
-				className='h-full w-full'
-				disabled={disabled}
-			>
-				<figure>
-					<img
-						ref={ref}
-						src={src}
-						{...props}
-					/>
-					{caption ? <figcaption>{caption}</figcaption> : null}
-				</figure>
-			</DialogTrigger>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>{src}</DialogTitle>
-				</DialogHeader>
-				<img
-					src={src}
-					{...props}
-				/>
-			</DialogContent>
-		</Dialog>
+		<figure ref={ref}>
+			<img {...props} />
+			{caption ? <figcaption>{caption}</figcaption> : null}
+		</figure>
 	);
 });
 
