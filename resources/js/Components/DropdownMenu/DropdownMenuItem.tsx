1ae9c336244edd727665caf36e5df33b25aacbd1
Narsilien <rigaux.jonathan@gmail.com>
2024-08-25 19:19:07 +0200

    Add active to dropdown menu item

--- resources/js/Components/DropdownMenu/DropdownMenuItem.tsx
+++ resources/js/Components/DropdownMenu/DropdownMenuItem.tsx
@@ -3,14 +3,16 @@ import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import * as React from "react";
 
 export interface DropdownMenuItemProps extends React.ComponentProps<typeof DropdownMenuPrimitive.Item> {
+	active?: boolean;
 	inset?: boolean;
 }
 
 const DropdownMenuItem = React.forwardRef<React.ElementRef<typeof DropdownMenuPrimitive.Item>, DropdownMenuItemProps>(
-	({ className, inset, ...props }, ref) => (
+	({ active, className, inset, ...props }, ref) => (
 		<DropdownMenuPrimitive.Item
 			ref={ref}
 			className={cn(
+				active && "font-semibold text-primary-highlight",
 				"relative flex cursor-default select-none items-center gap-x-2 rounded-sm px-2 py-1.5 text-sm outline-none transition-colors",
 				"focus:bg-accent focus:text-accent-foreground",
 				"data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
