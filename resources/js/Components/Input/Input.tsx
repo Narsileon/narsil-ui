bd47698d8cc951de3fedb0305faa7f4099d5702b
Narsilien <rigaux.jonathan@gmail.com>
2024-08-25 18:47:36 +0200

    Readd focus rings

--- resources/js/Components/Input/Input.tsx
+++ resources/js/Components/Input/Input.tsx
@@ -10,7 +10,7 @@ export const inputStyle = cva(
 		"border-input bg-background ring-offset-background flex h-10 w-full rounded-md border px-4 py-2 text-sm",
 		"placeholder:text-muted-foreground",
 		"file:border-0 file:bg-transparent file:text-sm file:font-medium",
-		"focus-visible:border-primary focus-visible:outline-none",
+		"focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-highlight focus-visible:ring-offset-2",
 		"disabled:cursor-not-allowed disabled:opacity-50"
 	)
 );
