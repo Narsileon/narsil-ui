b2c6974a797e6cb3b9cd14b684b9beb7ee165b9f
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-08-30 10:43:51 +0200

    Correct input search warning

--- resources/js/Components/Input/InputSearch.tsx
+++ resources/js/Components/Input/InputSearch.tsx
@@ -10,7 +10,7 @@ export interface InputSearchProps
 		Pick<InputProps, "value" | "onChange"> {}
 
 const InputSearch = React.forwardRef<HTMLDivElement, InputSearchProps>(
-	({ className, value, onChange, ...props }, ref) => {
+	({ className, id = "search", value, onChange, ...props }, ref) => {
 		const { trans } = useTranslationsStore();
 
 		return (
@@ -22,6 +22,7 @@ const InputSearch = React.forwardRef<HTMLDivElement, InputSearchProps>(
 				<Search className='botton-1.5 z-1 absolute left-1.5 top-1.5 h-6 w-6' />
 
 				<Input
+					id={id}
 					className='w-full px-9'
 					placeholder={trans("Search...")}
 					value={value}
