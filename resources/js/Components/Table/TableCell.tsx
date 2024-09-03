9da7cec90c3e35039a939740add80e54b174cfec
Narsilien <rigaux.jonathan@gmail.com>
2024-09-03 19:25:01 +0200

    Reduce table padding

--- resources/js/Components/Table/TableCell.tsx
+++ resources/js/Components/Table/TableCell.tsx
@@ -6,7 +6,7 @@ export interface TableCellProps extends React.TdHTMLAttributes<HTMLTableCellElem
 const TableCell = React.forwardRef<HTMLTableCellElement, TableCellProps>(({ className, ...props }, ref) => (
 	<td
 		ref={ref}
-		className={cn("h-10 px-4 py-1 align-middle [&:has([role=checkbox])]:pr-0", className)}
+		className={cn("h-10 px-3 py-1 align-middle [&:has([role=checkbox])]:pr-0", className)}
 		{...props}
 	/>
 ));
