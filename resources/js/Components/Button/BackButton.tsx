1034e8931cb414381051097ccbb464eda1d30c1d
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-09-03 12:18:32 +0200

    Update backbutton to use alert

--- resources/js/Components/Button/BackButton.tsx
+++ resources/js/Components/Button/BackButton.tsx
@@ -1,9 +1,18 @@
 import { ChevronLeft } from "lucide-react";
-import { router } from "@inertiajs/react";
+import { Link, router } from "@inertiajs/react";
 import { useTranslationsStore } from "@narsil-localization/Stores/translationStore";
 import * as React from "react";
+import AlertDialog from "@narsil-ui/Components/AlertDialog/AlertDialog";
+import AlertDialogAction from "@narsil-ui/Components/AlertDialog/AlertDialogAction";
+import AlertDialogCancel from "@narsil-ui/Components/AlertDialog/AlertDialogCancel";
+import AlertDialogContent from "@narsil-ui/Components/AlertDialog/AlertDialogContent";
+import AlertDialogDescription from "@narsil-ui/Components/AlertDialog/AlertDialogDescription";
+import AlertDialogFooter from "@narsil-ui/Components/AlertDialog/AlertDialogFooter";
+import AlertDialogHeader from "@narsil-ui/Components/AlertDialog/AlertDialogHeader";
+import AlertDialogTitle from "@narsil-ui/Components/AlertDialog/AlertDialogTitle";
+import AlertDialogTrigger from "@narsil-ui/Components/AlertDialog/AlertDialogTrigger";
 import Button, { ButtonProps } from "@narsil-ui/Components/Button/Button";
-import TooltipWrapper from "../Tooltip/TooltipWrapper";
+import TooltipWrapper from "@narsil-ui/Components/Tooltip/TooltipWrapper";
 
 interface BackButtonProps extends ButtonProps {
 	asIcon?: boolean;
@@ -15,44 +24,70 @@ const BackButton = React.forwardRef<HTMLButtonElement, BackButtonProps>(
 	({ asIcon = false, href, isDirty = false, ...props }, ref) => {
 		const { trans } = useTranslationsStore();
 
-		const message = trans("Do you really want to leave this page? Unsaved data has been detected.");
-
-		const onClick = () => {
-			if (isDirty) {
-				if (!confirm(message)) {
-					return;
-				}
-			}
-
-			router.visit(href);
-		};
+		const [open, setOpen] = React.useState(false);
 
 		return href ? (
-			asIcon ? (
-				<TooltipWrapper tooltip={trans("common.back")}>
-					<Button
-						ref={ref}
-						size={"icon"}
-						type='button'
-						variant='secondary'
-						onClick={onClick}
-						{...props}
-					>
-						<ChevronLeft className='h-6 w-6' />
-						<span className='sr-only'>{trans("common.back")}</span>
-					</Button>
-				</TooltipWrapper>
-			) : (
-				<Button
-					ref={ref}
-					type='button'
-					variant='secondary'
-					onClick={onClick}
-					{...props}
-				>
-					{trans("common.back")}
-				</Button>
-			)
+			<AlertDialog
+				open={open}
+				onOpenChange={(value) => {
+					if (href) {
+						setOpen(value);
+					}
+				}}
+			>
+				<AlertDialogTrigger asChild={true}>
+					{asIcon ? (
+						<TooltipWrapper tooltip={trans("common.back")}>
+							<Button
+								ref={ref}
+								size={"icon"}
+								type='button'
+								variant='secondary'
+								onClick={() => {
+									if (!isDirty) {
+										router.visit(href);
+									}
+								}}
+								{...props}
+							>
+								<ChevronLeft className='h-6 w-6' />
+								<span className='sr-only'>{trans("common.back")}</span>
+							</Button>
+						</TooltipWrapper>
+					) : (
+						<Button
+							ref={ref}
+							type='button'
+							variant='secondary'
+							onClick={() => {
+								if (!isDirty) {
+									router.visit(href);
+								}
+							}}
+							{...props}
+						>
+							{trans("common.back")}
+						</Button>
+					)}
+				</AlertDialogTrigger>
+				<AlertDialogContent>
+					<AlertDialogHeader>
+						<AlertDialogTitle>{trans("Are you sure?")}</AlertDialogTitle>
+					</AlertDialogHeader>
+					<AlertDialogDescription>{trans("Any change will be lost.")}</AlertDialogDescription>
+					<AlertDialogFooter>
+						<AlertDialogCancel>{trans("Cancel")}</AlertDialogCancel>
+						<AlertDialogAction asChild={true}>
+							<Link
+								as='button'
+								href={href}
+							>
+								{trans("Continue")}
+							</Link>
+						</AlertDialogAction>
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialog>
 		) : null;
 	}
 );
