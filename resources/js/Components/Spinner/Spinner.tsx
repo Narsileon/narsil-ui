f75484f6866e52512d8e2d574b6435c2dbd0c71a
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-09-03 13:47:18 +0200

    Update spinner

--- resources/js/Components/Spinner/Spinner.tsx
+++ resources/js/Components/Spinner/Spinner.tsx
@@ -1,44 +1,14 @@
 import { cn } from "@narsil-ui/Components";
-import { cva, VariantProps } from "class-variance-authority";
-import { Loader2 } from "lucide-react";
-import * as React from "react";
+import { Loader2, LucideProps } from "lucide-react";
 
-const spinnerVariants = cva("flex-col items-center justify-center", {
-	variants: {
-		show: {
-			true: "flex",
-			false: "hidden",
-		},
-	},
-	defaultVariants: {
-		show: true,
-	},
-});
+interface SpinnerProps extends LucideProps {}
 
-const loaderVariants = cva("animate-spin text-primary", {
-	variants: {
-		size: {
-			small: "h-4 w-4",
-			medium: "h-6 w-6",
-			large: "h-8 w-8",
-		},
-	},
-	defaultVariants: {
-		size: "medium",
-	},
-});
-
-interface SpinnerProps extends VariantProps<typeof spinnerVariants>, VariantProps<typeof loaderVariants> {
-	className?: string;
-	children?: React.ReactNode;
-}
-
-const Spinner = ({ children, className, size, show }: SpinnerProps) => {
+const Spinner = ({ className, ...props }: SpinnerProps) => {
 	return (
-		<span className={spinnerVariants({ show })}>
-			<Loader2 className={cn(loaderVariants({ size }), className)} />
-			{children}
-		</span>
+		<Loader2
+			className={cn("h-6 w-6 animate-spin", className)}
+			{...props}
+		/>
 	);
 };
 
