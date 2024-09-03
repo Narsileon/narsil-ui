0ff2e7e271b18c24874700852b81d53efcd91ef0
Jonathan Rigaux <49496020+Narsileon@users.noreply.github.com>
2024-09-03 11:54:15 +0200

    Alloy icon on backbutton

--- resources/js/Components/Button/BackButton.tsx
+++ resources/js/Components/Button/BackButton.tsx
@@ -2,38 +2,43 @@ import { router } from "@inertiajs/react";
 import { useTranslationsStore } from "@narsil-localization/Stores/translationStore";
 import * as React from "react";
 import Button, { ButtonProps } from "@narsil-ui/Components/Button/Button";
+import { ChevronLeft } from "lucide-react";
 
 interface BackButtonProps extends ButtonProps {
+	asIcon?: boolean;
 	isDirty?: boolean;
 	href: string;
 }
 
-const BackButton = React.forwardRef<HTMLButtonElement, BackButtonProps>(({ href, isDirty = false, ...props }, ref) => {
-	const { trans } = useTranslationsStore();
+const BackButton = React.forwardRef<HTMLButtonElement, BackButtonProps>(
+	({ asIcon = false, href, isDirty = false, ...props }, ref) => {
+		const { trans } = useTranslationsStore();
 
-	const message = trans("Do you really want to leave this page? Unsaved data has been detected.");
+		const message = trans("Do you really want to leave this page? Unsaved data has been detected.");
 
-	const onClick = () => {
-		if (isDirty) {
-			if (!confirm(message)) {
-				return;
+		const onClick = () => {
+			if (isDirty) {
+				if (!confirm(message)) {
+					return;
+				}
 			}
-		}
 
-		router.visit(href);
-	};
+			router.visit(href);
+		};
 
-	return href ? (
-		<Button
-			ref={ref}
-			type='button'
-			variant='secondary'
-			onClick={onClick}
-			{...props}
-		>
-			{trans("common.back")}
-		</Button>
-	) : null;
-});
+		return href ? (
+			<Button
+				ref={ref}
+				size={asIcon ? "icon" : "default"}
+				type='button'
+				variant='secondary'
+				onClick={onClick}
+				{...props}
+			>
+				{asIcon ? <ChevronLeft className='h-6 w-6' /> : trans("common.back")}
+			</Button>
+		) : null;
+	}
+);
 
 export default BackButton;
