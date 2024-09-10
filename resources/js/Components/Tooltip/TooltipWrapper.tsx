85f7dda2f787eb7ee85b59c01e1dbc97e6d53ea3
Narsilien <rigaux.jonathan@gmail.com>
2024-09-10 09:31:40 +0200

    Improve tooltipwrapper

--- resources/js/Components/Tooltip/TooltipWrapper.tsx
+++ resources/js/Components/Tooltip/TooltipWrapper.tsx
@@ -4,23 +4,37 @@ import Tooltip from "./Tooltip";
 import TooltipContent from "./TooltipContent";
 import TooltipProvider from "./TooltipProvider";
 import TooltipTrigger from "./TooltipTrigger";
+import type { TooltipContentProps } from "./TooltipContent";
+import type { TooltipTriggerProps } from "./TooltipTrigger";
 
-export interface TooltipWrapperProps {
+export interface TooltipWrapperProps extends TooltipTriggerProps {
+	align: TooltipContentProps["align"];
+	alignOffset: TooltipContentProps["alignOffset"];
 	children: React.ReactNode;
+	side: TooltipContentProps["side"];
+	sideOffset: TooltipContentProps["sideOffset"];
 	tooltip: string | React.ReactNode;
 }
 
 const TooltipWrapper = React.forwardRef<React.ElementRef<typeof TooltipPrimitive.Trigger>, TooltipWrapperProps>(
-	({ children, tooltip }, ref) => (
+	({ align, alignOffset, children, side, sideOffset, tooltip, ...props }, ref) => (
 		<TooltipProvider>
 			<Tooltip>
 				<TooltipTrigger
 					ref={ref}
 					asChild={true}
+					{...props}
 				>
 					{children}
 				</TooltipTrigger>
-				<TooltipContent>{tooltip}</TooltipContent>
+				<TooltipContent
+					align={align}
+					alignOffset={alignOffset}
+					side={side}
+					sideOffset={sideOffset}
+				>
+					{tooltip}
+				</TooltipContent>
 			</Tooltip>
 		</TooltipProvider>
 	)
