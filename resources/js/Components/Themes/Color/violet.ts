c5892b4e49228731cb26e04bc5d4cc87f9d05edd
Narsilien <rigaux.jonathan@gmail.com>
2024-09-11 14:21:24 +0200

    Rework colors

--- resources/js/Components/Themes/Color/violet.ts
+++ resources/js/Components/Themes/Color/violet.ts
@@ -3,52 +3,14 @@ import { baseDark, baseLight, Theme } from "./color";
 const violet: Theme = {
 	light: {
 		...baseLight,
-		"accent-foreground": "221 39% 11%",
-		"card-foreground": "224 71% 4%",
-		"constructive-foreground": "210 20% 98%",
-		"destructive-foreground": "210 20% 98%",
-		"muted-foreground": "220 9% 46%",
-		"popover-foreground": "224 71% 4%",
-		"primary-foreground": "210 20% 98%",
-		"primary-highlight-foreground": "210 20% 98%",
 		"primary-highlight": "262.1 83.3% 57.8%", // violet-600
-		"secondary-foreground": "221 39% 11%",
-		"secondary-highlight-foreground": "221 39% 11%",
-		"secondary-highlight": "220 14% 96%",
-		accent: "220 14% 96%",
-		border: "220 13% 91%",
-		constructive: "142 71% 45%",
-		destructive: "0 84% 60%",
-		foreground: "224 71% 4%",
-		muted: "220 14% 96%",
-		primary: "263.5 67.4% 34.9%", // violet-900
-		secondary: "220 14% 96%",
+		primary: "263.4 69.3% 42.2%", // violet-800
 	},
 	dark: {
 		...baseDark,
-		"accent-foreground": "210 20% 98%",
-		"card-foreground": "210 20% 98%",
-		"constructive-foreground": "210 20% 98%",
-		"destructive-foreground": "210 20% 98%",
-		"muted-foreground": "218 11% 65%",
-		"popover-foreground": "210 20% 98%",
-		"primary-foreground": "210 20% 98%",
-		"primary-highlight-foreground": "210 20% 98%",
 		"primary-highlight": "262.1 83.3% 57.8%", // violet-600
-		"secondary-foreground": "210 20% 98%",
-		"secondary-highlight-foreground": "210 20% 98%",
-		"secondary-highlight": "215 28% 17%",
-		accent: "215 28% 17%",
-		background: "224 71% 4%",
-		border: "215 28% 17%",
-		card: "224 71% 4%",
-		constructive: "144, 61%, 20%",
-		destructive: "0 63% 31%",
-		foreground: "210 20% 98%",
-		muted: "215 28% 17%",
-		popover: "224 71% 4%",
-		primary: "263.5 67.4% 34.9%", // violet-900
-		secondary: "215 28% 17%",
+		background: "275 66.67% 3.53%", // violet-995
+		primary: "263.4 69.3% 42.2%", // violet-800
 	},
 };
 
--- resources/js/Components/Themes/Color/yellow.ts
+++ resources/js/Components/Themes/Color/yellow.ts
@@ -3,52 +3,18 @@ import { baseDark, baseLight, Theme } from "./color";
 const yellow: Theme = {
 	light: {
 		...baseLight,
-		"accent-foreground": "24 10% 10%",
-		"card-foreground": "20 14% 4%",
-		"constructive-foreground": "60 9% 98%",
-		"destructive-foreground": "60 9% 98%",
-		"muted-foreground": "25 5% 45%",
-		"popover-foreground": "20 14% 4%",
-		"primary-foreground": "26 83% 14%",
-		"primary-highlight-foreground": "26 83% 14%",
-		"primary-highlight": "47.9 95.8% 53.1%", // yellow-400
-		"secondary-foreground": "24 10% 10%",
-		"secondary-highlight-foreground": "24 10% 10%",
-		"secondary-highlight": "60 5% 96%",
-		accent: "60 5% 96%",
-		border: "20 6% 90%",
-		constructive: "142 71% 45%",
-		destructive: "0 84% 60%",
-		foreground: "20 14% 4%",
-		muted: "60 5% 96%",
+		"primary-foreground": "222.2 84% 4.9%", // slate-950
+		"primary-highlight-foreground": "222.2 84% 4.9%", // slate-950
+		"primary-highlight": "50.4 97.8% 63.5%", // yellow-300
 		primary: "45.4 93.4% 47.5%", // yellow-500
-		secondary: "60 5% 96%",
 	},
 	dark: {
 		...baseDark,
-		"accent-foreground": "60 9% 98%",
-		"card-foreground": "60 9% 98%",
-		"constructive-foreground": "60 9% 98%",
-		"destructive-foreground": "60 9% 98%",
-		"muted-foreground": "24 5% 64%",
-		"popover-foreground": "60 9% 98%",
-		"primary-foreground": "26 83% 14%",
-		"primary-highlight-foreground": "26 83% 14%",
-		"primary-highlight": "47.9 95.8% 53.1%", // yellow-400
-		"secondary-foreground": "60 9% 98%",
-		"secondary-highlight-foreground": "60 9% 98%",
-		"secondary-highlight": "12 7% 15%",
-		accent: "12 7% 15%",
-		background: "20 14% 4%",
-		border: "12 7% 15%",
-		card: "20 14% 4%",
-		constructive: "144, 61%, 20%",
-		destructive: "0 63% 31%",
-		foreground: "60 9% 98%",
-		muted: "12 7% 15%",
-		popover: "20 14% 4%",
+		"primary-foreground": "222.2 84% 4.9%", // slate-950
+		"primary-highlight-foreground": "222.2 84% 4.9%", // slate-950
+		"primary-highlight": "50.4 97.8% 63.5%", // yellow-300
+		background: "21.82 84.62% 2.55%", //yellow-995
 		primary: "45.4 93.4% 47.5%", // yellow-500
-		secondary: "12 7% 15%",
 	},
 };
 
