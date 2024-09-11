0589546907da8f549dc0ed3fa7ddc14e384a4a4a
Narsilien <rigaux.jonathan@gmail.com>
2024-09-11 08:39:39 +0200

    Correct focus

--- resources/js/Components/Input/Input.tsx
+++ resources/js/Components/Input/Input.tsx
@@ -7,11 +7,11 @@ import TimePicker from "@narsil-ui/Components/Input/Time/TimePicker";
 
 export const inputStyle = cva(
 	cn(
-		"border-border bg-background ring-offset-background flex h-10 w-full rounded-md border px-3.5 py-2 text-sm",
+		"flex h-10 w-full rounded-md border border-border bg-background px-3.5 py-2 text-sm ring-offset-background",
 		"placeholder:text-muted-foreground",
 		"file:border-0 file:bg-transparent file:text-sm file:font-medium",
-		"focus-visible:outline-none focus-visible:border-primary-highlight",
-		"disabled:cursor-not-allowed disabled:opacity-50"
+		"focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2",
+		"disabled:opacity-50s disabled:cursor-not-allowed"
 	)
 );
 
