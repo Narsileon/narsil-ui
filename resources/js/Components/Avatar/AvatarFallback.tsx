aa8deeb81cbd9e3fb699eae75ea59d096e205388
Narsilien <rigaux.jonathan@gmail.com>
2024-09-11 09:12:44 +0200

    Add border to avatar fallback

--- resources/js/Components/Avatar/AvatarFallback.tsx
+++ resources/js/Components/Avatar/AvatarFallback.tsx
@@ -8,7 +8,10 @@ const AvatarFallback = React.forwardRef<React.ElementRef<typeof AvatarPrimitive.
 	({ className, ...props }, ref) => (
 		<AvatarPrimitive.Fallback
 			ref={ref}
-			className={cn("flex h-full w-full items-center justify-center rounded-full bg-muted", className)}
+			className={cn(
+				"flex h-full w-full items-center justify-center rounded-full border border-border bg-muted",
+				className
+			)}
 			{...props}
 		/>
 	)
