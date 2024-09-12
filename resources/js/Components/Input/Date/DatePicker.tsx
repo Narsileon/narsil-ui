2fc31b5a449437ddaf5aa6ce583bd7472e1c0d44
Narsilien <rigaux.jonathan@gmail.com>
2024-09-12 10:38:31 +0200

    Fix date picker

--- resources/js/Components/Input/Date/DatePicker.tsx
+++ resources/js/Components/Input/Date/DatePicker.tsx
@@ -1,22 +1,30 @@
 import { CalendarIcon } from "lucide-react";
 import { cn } from "@narsil-ui/Components";
+import { format } from "date-fns";
 import { InputProps, inputStyle } from "@narsil-ui/Components/Input/Input";
+import { useDatetimeLocale } from "@narsil-ui/Components/Input/Datetime/datetimeUtils";
+import { usePage } from "@inertiajs/react";
 import { useTranslationsStore } from "@narsil-localization/Stores/translationStore";
 import Button from "@narsil-ui/Components/Button/Button";
 import Calendar from "@narsil-ui/Components/Calendar/Calendar";
 import Popover from "@narsil-ui/Components/Popover/Popover";
 import PopoverContent from "@narsil-ui/Components/Popover/PopoverContent";
 import PopoverTrigger from "@narsil-ui/Components/Popover/PopoverTrigger";
+import type { GlobalProps } from "@narsil-ui/Types";
 
 export interface DatePickerProps extends InputProps {}
 
 const DatePicker = ({ className, id, required, value, onChange }: DatePickerProps) => {
-	const { locale, trans } = useTranslationsStore();
+	const { trans } = useTranslationsStore();
+
+	const { locale } = usePage<GlobalProps>().props.shared.localization;
+
+	const datetimeLocale = useDatetimeLocale(locale);
 
 	const selected = value ? new Date(value as string) : new Date();
 
 	const onSelect = (date: Date) => {
-		const nextValue = date.toISOString();
+		const nextValue = format(date, "yyyy-MM-dd");
 
 		const event = {
 			target: {
@@ -35,7 +43,7 @@ const DatePicker = ({ className, id, required, value, onChange }: DatePickerProp
 					className={cn(inputStyle(), "justify-start gap-x-2 font-normal", className)}
 				>
 					<CalendarIcon className='h-4 w-4' />
-					{value ? selected.toLocaleDateString(locale) : <span>{trans("Select...")}</span>}
+					{value ? format(selected, "P", { locale: datetimeLocale }) : <span>{trans("Select...")}</span>}
 				</Button>
 			</PopoverTrigger>
 			<PopoverContent className='w-auto p-0'>
