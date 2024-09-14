882d6bd2bc5c5392d2abd5452c1b8e1652285d52
Narsilien <rigaux.jonathan@gmail.com>
2024-09-14 21:01:27 +0200

    Update breadcrumbs

--- resources/js/Components/Breadcrumb/Breadcrumb.tsx
+++ resources/js/Components/Breadcrumb/Breadcrumb.tsx
@@ -1,12 +1,14 @@
+import { cn } from "@narsil-ui/Components/utils";
 import * as React from "react";
 
 export interface BreadcrumbProps extends React.HTMLAttributes<HTMLElement> {
 	separator?: React.ReactNode;
 }
 
-const Breadcrumb = React.forwardRef<HTMLElement, BreadcrumbProps>(({ ...props }, ref) => (
+const Breadcrumb = React.forwardRef<HTMLElement, BreadcrumbProps>(({ className, ...props }, ref) => (
 	<nav
 		ref={ref}
+		className={cn("flex h-10 items-center", className)}
 		aria-label='breadcrumb'
 		{...props}
 	/>
