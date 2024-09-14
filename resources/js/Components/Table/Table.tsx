cdef2324f4fef900c4f644c4ba4fa28bda9794f1
Narsilien <rigaux.jonathan@gmail.com>
2024-09-14 21:40:47 +0200

    Fix table

--- resources/js/Components/Table/Table.tsx
+++ resources/js/Components/Table/Table.tsx
@@ -6,7 +6,7 @@ export interface TableProps extends React.HTMLAttributes<HTMLTableElement> {}
 const Table = React.forwardRef<HTMLTableElement, TableProps>(({ className, ...props }, ref) => (
 	<table
 		ref={ref}
-		className={cn("min-w-max table-auto caption-bottom text-sm", className)}
+		className={cn("min-w-max table-auto caption-bottom text-sm w-full", className)}
 		{...props}
 	/>
 ));
