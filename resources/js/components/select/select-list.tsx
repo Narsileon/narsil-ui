49343d9ab386935a8a8eca975d082e7aab00865e
Narsilien <rigaux.jonathan@gmail.com>
2026-02-07 18:35:11 +0100

    Add missing class

--- resources/js/components/select/select-list.tsx
+++ resources/js/components/select/select-list.tsx
@@ -2,7 +2,7 @@ import { Select } from "@base-ui/react/select";
 import { cn } from "@narsil-ui/lib/utils";
 
 function SelectList({ className, ...props }: Select.List.Props) {
-  return <Select.List data-slot="select-list" className={cn("p-1")} {...props} />;
+  return <Select.List data-slot="select-list" className={cn("p-1", className)} {...props} />;
 }
 
 export default SelectList;
