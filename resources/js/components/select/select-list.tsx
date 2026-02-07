66a3743abdbeb0189b064a96ae41e963e8b668c0
Narsilien <rigaux.jonathan@gmail.com>
2026-02-07 18:30:33 +0100

    Fix select list padding

--- resources/js/components/select/select-list.tsx
+++ resources/js/components/select/select-list.tsx
@@ -1,7 +1,8 @@
 import { Select } from "@base-ui/react/select";
+import { cn } from "@narsil-ui/lib/utils";
 
-function SelectList({ ...props }: Select.List.Props) {
-  return <Select.List data-slot="select-list" {...props} />;
+function SelectList({ className, ...props }: Select.List.Props) {
+  return <Select.List data-slot="select-list" className={cn("p-1")} {...props} />;
 }
 
 export default SelectList;
