24af7feca3d75f6a16311b8f7e19bea466e85736
Narsilien <rigaux.jonathan@gmail.com>
2026-02-08 13:44:21 +0100

    Simplify combobox items

--- resources/js/components/combobox/combobox-list-item.tsx
+++ resources/js/components/combobox/combobox-list-item.tsx
@@ -1,29 +1,28 @@
 import { ItemContent, ItemDescription, ItemRoot, ItemTitle } from "@narsil-ui/components/item";
-import { cn } from "@narsil-ui/lib/utils";
 import parse from "html-react-parser";
 import { ComponentProps } from "react";
+import ComboboxItem from "./combobox-item";
 
-type ComboboxListItemProps = ComponentProps<typeof ItemRoot> & {
+type ComboboxListItemProps = ComponentProps<typeof ComboboxItem> & {
   displayValue?: boolean;
   label: string;
   value: string;
 };
 
-function ComboboxListItem({
-  className,
-  displayValue = true,
-  label,
-  size = "xs",
-  value,
-  ...props
-}: ComboboxListItemProps) {
+function ComboboxListItem({ displayValue = true, label, value, ...props }: ComboboxListItemProps) {
   return (
-    <ItemRoot className={cn("p-0", className)} size={size} {...props}>
-      <ItemContent className="flex flex-row items-center justify-between">
-        <ItemTitle className="font-normal whitespace-nowrap">{parse(label)}</ItemTitle>
-        {displayValue && <ItemDescription>{value}</ItemDescription>}
-      </ItemContent>
-    </ItemRoot>
+    <ComboboxItem
+      value={value}
+      render={
+        <ItemRoot className="p-0" size="xs">
+          <ItemContent className="flex flex-row items-center justify-between">
+            <ItemTitle className="font-normal whitespace-nowrap">{parse(label)}</ItemTitle>
+            {displayValue && <ItemDescription>{value}</ItemDescription>}
+          </ItemContent>
+        </ItemRoot>
+      }
+      {...props}
+    />
   );
 }
 
