39a95cc1d282a588730f5a421d377ad9618af35f
Narsilien <rigaux.jonathan@gmail.com>
2026-02-15 09:33:47 +0100

    Improve data table input

--- resources/js/components/button-group/button-group-variants.ts
+++ resources/js/components/button-group/button-group-variants.ts
@@ -1,14 +1,31 @@
+import { cn } from "@narsil-ui/lib/utils";
 import { cva } from "class-variance-authority";
 
 const buttonGroupVariants = cva(
-  "flex w-fit items-stretch has-[>[data-slot=button-group]]:gap-2 [&>*]:focus-visible:relative [&>*]:focus-visible:z-10 has-[select[aria-hidden=true]:last-child]:[&>[data-slot=select-trigger]:last-of-type]:rounded-r-md [&>[data-slot=select-trigger]:not([class*='w-'])]:w-fit [&>input]:flex-1",
+  cn(
+    "flex w-fit items-stretch",
+    "[&>[data-slot=select-trigger]:not([class*='w-'])]:w-fit",
+    "[&>input]:flex-1",
+    "*:focus-visible:relative *:focus-visible:z-10",
+    "has-[>[data-slot=button-group]]:gap-2",
+    "has-[select[aria-hidden=true]:last-child]:[&>[data-slot=select-trigger]:last-of-type]:rounded-r-md",
+  ),
   {
     variants: {
       orientation: {
-        horizontal:
-          "[&>*:not(:first-child)]:rounded-l-none [&>*:not(:first-child)]:border-l-0 [&>*:not(:last-child)]:rounded-r-none",
-        vertical:
-          "flex-col [&>*:not(:first-child)]:rounded-t-none [&>*:not(:first-child)]:border-t-0 [&>*:not(:last-child)]:rounded-b-none",
+        horizontal: cn(
+          "*:data-slot:rounded-r-none",
+          "[&>[data-slot]:not(:has(~[data-slot]))]:rounded-r-md!",
+          "[&>[data-slot]~[data-slot]]:border-l-0",
+          "[&>[data-slot]~[data-slot]]:rounded-l-none",
+        ),
+        vertical: cn(
+          "flex-col",
+          "[&>[data-slot]:not(:has(~[data-slot]))]:rounded-b-md!",
+          "[&>[data-slot]~[data-slot]]:border-t-0",
+          "[&>[data-slot]~[data-slot]]:rounded-t-none",
+          "*:data-slot:rounded-b-none",
+        ),
       },
     },
     defaultVariants: {
--- resources/js/components/data-table/data-table-input.tsx
+++ resources/js/components/data-table/data-table-input.tsx
@@ -1,4 +1,6 @@
-import { useDataTable } from "@narsil-ui/components/data-table";
+import { Button } from "@narsil-ui/components/button";
+import { ButtonGroup } from "@narsil-ui/components/button-group";
+import { DataTableColumnFilters, useDataTable } from "@narsil-ui/components/data-table";
 import { Icon } from "@narsil-ui/components/icon";
 import { InputContent } from "@narsil-ui/components/input";
 import {
@@ -7,6 +9,7 @@ import {
   InputGroupButton,
   InputGroupInput,
 } from "@narsil-ui/components/input-group";
+import { Tooltip } from "@narsil-ui/components/tooltip";
 import { useTranslator } from "@narsil-ui/components/translator";
 import { debounce } from "lodash-es";
 import { useEffect, useMemo, useState, type ComponentProps } from "react";
@@ -32,25 +35,38 @@ function DataTableInput({ className, ...props }: DataTableInputProps) {
     debouncedSetValue(value);
   }, [value, debouncedSetValue]);
 
+  const filterLabel = trans("data-table.filters", { fallback: "Filters" });
+
   return (
-    <InputGroup className="max-w-3xs place-self-center transition-all duration-300 focus-within:max-w-lg">
-      <InputGroupAddon>
-        <Icon name="search" />
-      </InputGroupAddon>
-      <InputGroupInput
-        placeholder={trans("placeholders.search")}
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-        {...props}
-      />
-      {table.getState().globalFilter ? (
-        <InputGroupAddon align="inline-end">
-          <InputGroupButton size="icon-sm" onClick={() => setValue("")}>
-            <Icon name="x" />
-          </InputGroupButton>
+    <ButtonGroup className="grow justify-end">
+      <InputGroup className="max-w-3xs place-self-center transition-all duration-300 focus-within:max-w-lg">
+        <InputGroupAddon>
+          <Icon name="search" />
         </InputGroupAddon>
-      ) : null}
-    </InputGroup>
+        <InputGroupInput
+          placeholder={trans("placeholders.search")}
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+          {...props}
+        />
+        {table.getState().globalFilter ? (
+          <InputGroupAddon align="inline-end">
+            <InputGroupButton size="icon-sm" onClick={() => setValue("")}>
+              <Icon name="x" />
+            </InputGroupButton>
+          </InputGroupAddon>
+        ) : null}
+      </InputGroup>
+      <Tooltip tooltip={filterLabel}>
+        <DataTableColumnFilters
+          render={
+            <Button aria-label={filterLabel} className="border-secondary" variant="secondary">
+              <Icon name="filter" />
+            </Button>
+          }
+        />
+      </Tooltip>
+    </ButtonGroup>
   );
 }
 
