8b49fd0ddb818305d021daea3b066a5367aa3723
Jonathan Rigaux <jonathan.rigaux@rheinschafe.de>
2026-02-16 14:38:35 +0100

    Simplify theme

--- resources/js/components/tabs/tabs-tab.tsx
+++ resources/js/components/tabs/tabs-tab.tsx
@@ -9,10 +9,10 @@ function TabsTab({ className, ...props }: Tabs.Tab.Props) {
         "inline-flex h-9 shrink-0 cursor-pointer items-center gap-2 rounded-md border border-transparent px-3 py-2 whitespace-nowrap text-foreground transition-[color,box-shadow]",
         "disabled:pointer-events-none disabled:opacity-50",
         "focus-visible:border-shine",
-        "hover:bg-sidebar-accent hover:text-sidebar-accent-foreground",
+        "hover:bg-accent hover:text-accent-foreground",
         "data-[orientation=horizontal]:justify-center data-[orientation=vertical]:justify-start",
         "data-[orientation=vertical]:w-full",
-        "data-active:bg-sidebar-accent",
+        "data-active:bg-accent",
         "[&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className,
       )}
