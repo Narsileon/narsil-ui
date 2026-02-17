364c9b739455a0fd56659a27379094e5892b7295
Narsilien <rigaux.jonathan@gmail.com>
2026-02-17 18:07:56 +0100

    Add types

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -14,8 +14,41 @@ export type DataTableCollection<T = Data> = {
   };
 };
 
-export type RoutesData = Record<string, string> & {
-  parameters?: Record<string, unknown>;
+export type FieldsetData = {
+  id?: string | null;
+  label?: string | null;
+  description?: string | null;
+  elements: Array<FieldsetData | InputData>;
+};
+
+export type FormStepData = {
+  id?: string | null;
+  label?: string | null;
+  description?: string | null;
+  elements: Array<FieldsetData | InputData>;
+};
+
+export type InputData = {
+  id: string;
+  label?: string | null;
+  description?: string | null;
+  append?: string | null;
+  icon?: string | null;
+  translatable: boolean;
+  width: number;
+  type: string;
+  options?: OptionData[] | null;
+  props: {
+    accept?: string | null;
+    autoComplete?: string | null;
+    className?: string | null;
+    id: string;
+    placeholder?: string | null;
+    readonly?: boolean;
+    required?: boolean;
+    type: string;
+    [key: string]: any;
+  } | null;
 };
 
 export type OptionData = {
@@ -23,6 +56,10 @@ export type OptionData = {
   icon?: IconName;
   label: string | Record<string, string>;
   value: unknown;
+};
+
+export type RoutesData = Record<string, string> & {
+  parameters?: Record<string, unknown>;
 };
 
 export type TableData = {
--- src/Http/Data/Forms/FormStepData.php
+++ src/Http/Data/Forms/FormStepData.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Fluent;
  * @version 1.0.0
  * @author Jonathan Rigaux
  *
- * @property string|null $handle The handle of the step.
+ * @property string|null $id The id of the step.
  * @property string|null $label The label of the step.
  * @property string|null $description The description of the step.
  * @property array<FieldsetData|InputData> $elements The elements of the step.
@@ -22,7 +22,7 @@ class FormStepData extends Fluent
     #region CONSTRUCTOR
 
     /**
-     * @param string|null $handle
+     * @param string|null $id
      * @param string|null $label
      * @param string|null $description
      * @param array<FieldsetData|InputData> $elements
@@ -30,13 +30,13 @@ class FormStepData extends Fluent
      * @return void
      */
     public function __construct(
-        public ?string $handle = null,
+        public ?string $id = null,
         public ?string $label = null,
         public ?string $description = null,
         public array $elements = [],
     )
     {
-        $this->set('handle', $handle);
+        $this->set('id', $id);
         $this->set('label', $label);
         $this->set('description', $description);
         $this->set('elements', $elements);
