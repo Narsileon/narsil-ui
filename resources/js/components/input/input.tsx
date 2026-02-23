6a10bfb56cac4555843b581236b6ce545d85a831
Narsilien <rigaux.jonathan@gmail.com>
2026-02-23 17:47:59 +0100

    Fix input line height

--- resources/js/components/input/input.tsx
+++ resources/js/components/input/input.tsx
@@ -6,10 +6,10 @@ function Input({ className, type, ...props }: InputPrimitive.Props) {
     <InputPrimitive
       data-slot="input"
       className={cn(
-        "h-9 w-full min-w-0 rounded-lg border border-input bg-transparent px-2.5 py-1 text-base transition-colors outline-none md:text-sm dark:bg-input/30",
+        "h-9 w-full min-w-0 rounded-lg border border-input bg-transparent px-2.5 py-1 text-sm/7 transition-colors outline-none dark:bg-input/30",
         "aria-invalid:border-destructive aria-invalid:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40",
         "disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 dark:disabled:bg-input/80",
-        "file:inline-flex file:h-6 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground",
+        "file:inline-flex file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground",
         "focus-visible:shine-border",
         "placeholder:text-muted-foreground",
         className,
