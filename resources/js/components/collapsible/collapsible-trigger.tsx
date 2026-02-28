5540e2dc5732d8749fd99e93b67a782d0184ad39
Narsilien <rigaux.jonathan@gmail.com>
2026-02-28 09:57:32 +0100

    Remove cursor pointer from collapsible trigger

--- resources/js/components/collapsible/collapsible-trigger.tsx
+++ resources/js/components/collapsible/collapsible-trigger.tsx
@@ -1,14 +1,7 @@
 import { Collapsible } from "@base-ui/react/collapsible";
-import { cn } from "@narsil-ui/lib/utils";
 
-function CollapsibleTrigger({ className, ...props }: Collapsible.Trigger.Props) {
-  return (
-    <Collapsible.Trigger
-      data-slot="collapsible-trigger"
-      className={cn("cursor-pointer", className)}
-      {...props}
-    />
-  );
+function CollapsibleTrigger({ ...props }: Collapsible.Trigger.Props) {
+  return <Collapsible.Trigger data-slot="collapsible-trigger" {...props} />;
 }
 
 export default CollapsibleTrigger;
